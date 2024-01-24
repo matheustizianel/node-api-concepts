9f75aabdea16aed6ca89ef1e938850ba7f3a3eda
Matheus Tizianel <matheus9.mts@gmail.com>
2024-01-24 00:05:15 -0500

    GET routes finished and package.json modified

--- src/server.js
+++ src/server.js
@@ -3,4 +3,14 @@ import express from "express";
 const PORT = 3333;
 const app = express();
 
+app.get("/message/:id/:user", (req, res) => {
+  const { id, user } = req.params;
+  res.send(`Message ID: ${id} -------------- User name: ${user}`);
+});
+
+app.get("/users", (req, res) => {
+  const { page, limit } = req.query;
+  res.send(`Page: ${page} -------------- Limited to ${limit} items.`);
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
